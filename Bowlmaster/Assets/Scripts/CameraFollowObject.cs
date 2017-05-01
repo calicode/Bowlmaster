b0dc9a3ea79903ccebae1d3a0eace77ba193a2ef
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-05-01 18:40:26 -0500

    Prefabed pins. Added camera follow object script. Mucked with physics

---
+++ Bowlmaster/Assets/Scripts/CameraFollowObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowObject : MonoBehaviour
+{
+    public GameObject objectToFollow;
+    private Vector3 viewOffset;
+    private float cutoffPoint = 1800f; // before pin 1 in bowling game
+    void Start()
+    {
+        viewOffset = new Vector3(0, -30, 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < cutoffPoint)
+        {
+            transform.position = objectToFollow.transform.position - viewOffset;
+        }
+    }
+}
---
+++ Bowlmaster/Assets/Scripts/RollOnMouseClick.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollOnMouseClick : MonoBehaviour
+{
+    Rigidbody rb;
+    AudioSource asource;
+    bool rolled = false;
+    public float rollSpeed = 50;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        asource = GetComponent<AudioSource>();
+
+    }
+
+
+
+
+    void RollMe()
+    {
+        if (!rolled)
+        {
+            Debug.Log("got mouseclick");
+            rolled = true;
+            rb.velocity = new Vector3(0, 0, rollSpeed);
+            asource.Play();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RollMe();
+        }
+    }
+}
